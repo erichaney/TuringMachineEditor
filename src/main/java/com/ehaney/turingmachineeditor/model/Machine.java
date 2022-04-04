724754b417bae41e7bc151a4144579233de61c08
erichaney <ehaney@outlook.com>
2022-04-03 18:22:35 -0700

    Add javadoc comments

--- src/main/java/com/ehaney/turingmachineeditor/model/Machine.java
+++ src/main/java/com/ehaney/turingmachineeditor/model/Machine.java
@@ -7,47 +7,60 @@ import java.util.Map;
 
 /**
  * The data model of the Turing Machine.
- *
+ * <p>
  * A Turing Machine canonically consists of a tape and tape reader, a set of states,
  * an initial state, and a set of state transition functions. The machine also
  * keeps account of its history with stacks for is previous steps that can be
  * rolled back with undo and rolled forward with redo. It also keeps a reference
  * to a clone of its input tape and initial state in its InitialConfiguration
  * to facilitate rewinding the machine back to step 0.
- *
+ * <p>
  * A "Step" object is a simple record of how the machine has changed during a
  * single transition. Steps are used to roll back the state of the machine during
  * undo operations.
- *
  */
 public class Machine {
 
-    /** The number of times the machine has transitioned. */
+    /**
+     * The number of times the machine has transitioned.
+     */
     private int stepNumber;
 
-    /** The tape of symbols and tape head. */
+    /**
+     * The tape of symbols and tape head.
+     */
     private Tape tape;
 
-    /** The active state. */
+    /**
+     * The active state.
+     */
     private State currentState;
 
-    /** The map of all states keyed by their IDs. */
+    /**
+     * The map of all states keyed by their IDs.
+     */
     private Map<String, State> states;
 
-    /** A record of the initial tape and the initial state. */
+    /**
+     * A record of the initial tape and the initial state.
+     */
     private InitialConfiguration init;
 
-    /** A record of the current state transition. */
+    /**
+     * A record of the current state transition.
+     */
     private Step currentStep;
 
-    /** The undo and redo stack of steps. */
+    /**
+     * The undo and redo stack of steps.
+     */
     private Deque<Step> undoStack;
     private Deque<Step> redoStack;
 
     /**
      * Constructs a Turing Machine with a tape, one state, and no transitions.
      *
-     * @param initialTape The tape of the Turing Machine.
+     * @param initialTape  The tape of the Turing Machine.
      * @param initialState The initial state.
      */
     public Machine(Tape initialTape, State initialState) {
@@ -66,8 +79,8 @@ public class Machine {
      * Constructs a Turing Machine with the given tape string, one state, and no
      * transitions.
      *
-     * @param tapeString The space-separated symbols of the tape.
-     * @param stateID The ID of the initial state.
+     * @param tapeString  The space-separated symbols of the tape.
+     * @param stateID     The ID of the initial state.
      * @param stateAction The action of the initial state.
      */
     public Machine(String tapeString, String stateID, State.Action stateAction) {
