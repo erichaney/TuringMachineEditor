91fa4d261470aeca9a048c89eae07e0fef2a107d
erichaney <ehaney@outlook.com>
2022-04-03 18:23:51 -0700

    Add methods

--- src/main/java/com/ehaney/turingmachineeditor/model/Machine.java
+++ src/main/java/com/ehaney/turingmachineeditor/model/Machine.java
@@ -87,6 +87,141 @@ public class Machine {
         this(new Tape(tapeString), new State(stateID, stateAction));
     }
 
+    public Tape getTape() {
+        return tape;
+    }
+
+    public Map<String, State> getStates() {
+        return states;
+    }
+
+    public State getState(String id) {
+        return states.get(id);
+    }
+
+    public Machine addState(State s) {
+        if (!states.containsKey(s.getID())) {
+            states.put(s.getID(), s);
+        } else {
+            throw new IllegalArgumentException("Found another state with the same " +
+                    "id. States must have unique ids.");
+        }
+        return this;
+    }
+
+    public Machine addState(String id, State.Action action) {
+        return addState(new State(id, action));
+    }
+
+    public void setStateID(State state, String newID) {
+        if (states.containsKey(newID)) {
+            throw new IllegalArgumentException("Found another state with the " +
+                    "same id. States must have unique ids.");
+        } else {
+            states.remove(state.getID());
+            state.setID(newID);
+            addState(state);
+        }
+    }
+
+    public boolean isHalted() {
+        State.Action a = currentState.getAction();
+        return a == State.Action.H || a == State.Action.Y || a == State.Action.N;
+    }
+
+    private void transition() {
+        stepNumber++;
+
+        String readSymbol = tape.readSymbol();
+        Transition availableTransition = currentState.getTransition(readSymbol);
+
+        if (availableTransition != null) {
+            tape.writeSymbol(availableTransition.getWriteSymbol());
+            currentState = availableTransition.getToState();
+        }
+
+        if (currentState.getAction() == State.Action.L) {
+            tape.shiftLeft();
+        } else if (currentState.getAction() == State.Action.R) {
+            tape.shiftRight();
+        }
+    }
+
+    public void stepForward() {
+        if (isHalted()) {
+            return;
+        } else if (redoStack.size() > 0) {
+            redoStep();
+        } else {
+            transition();
+            undoStack.push(currentStep);
+            currentStep = new Step(tape, currentState);
+        }
+    }
+
+    public void undoStep() {
+        if (undoStack.size() == 0) {
+            return;
+        }
+        stepNumber--;
+        redoStack.push(currentStep);
+        currentStep = undoStack.pop();
+
+        // rewind using the information from the previous step
+        tape.setPosition(currentStep.tapePosition);
+        tape.writeSymbol(currentStep.readSymbol);
+        currentState = getState(currentStep.stateID);
+    }
+
+    public void redoStep() {
+        if (redoStack.size() == 0) {
+            return;
+        } else {
+           undoStack.push(currentStep);
+           currentStep = redoStack.pop();
+           transition();
+        }
+    }
+
+    public boolean removeState(String id) {
+        if (id.equals(init.initialState.getID())) {
+            throw new IllegalArgumentException("Removing the initial state is prohibited.");
+        } else if (states.remove(id) == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void addAllStates(State... states) {
+        for (State s : states) {
+            addState(s);
+        }
+    }
+
+    public String getCurrentStateID() {
+        return currentState.getID();
+    }
+
+    public void stepForward(int times) {
+        for (int i = 0; i < times; i++) {
+            stepForward();
+        }
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void reset() {
+        stepNumber = 0;
+        undoStack.clear();
+        redoStack.clear();
+        tape = init.initialTape;
+        currentState = init.initialState;
+        currentStep = new Step(tape, currentState);
+    }
+
     /**
      * This class holds a clone of the initial Tape and the initial state for
      * the purposes of efficiently resetting the machine.
