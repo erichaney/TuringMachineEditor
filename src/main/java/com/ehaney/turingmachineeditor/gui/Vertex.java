b6363b5bd43974c57d80ddd5603ec367dc920a1a
erichaney <ehaney@outlook.com>
2022-04-05 11:54:24 -0700

    Add context menus

--- src/main/java/com/ehaney/turingmachineeditor/gui/Vertex.java
+++ src/main/java/com/ehaney/turingmachineeditor/gui/Vertex.java
@@ -2,14 +2,19 @@ package com.ehaney.turingmachineeditor.gui;
 
 import com.ehaney.turingmachineeditor.model.State;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+
+import java.util.Arrays;
 
 public class Vertex extends StackPane {
     private State state;
@@ -23,27 +28,73 @@ public class Vertex extends StackPane {
 
     public Vertex(State state, double x, double y) {
         this.state = state;
+
+        // Configure circle
         icon = new Circle(25, Color.WHITE);
         icon.setStroke(Color.BLACK);
         icon.setStrokeWidth(2);
-        idLabel = new Label(state.getID());
+
+        // Configure labels
+        idLabel = new Label();
+        idLabel.textProperty().bind(state.idProperty());
         idLabel.setFont(idFont);
         idLabel.setTextFill(Color.GREEN);
-        actionLabel = new Label(state.getAction().toString());
+        actionLabel = new Label();
+        actionLabel.textProperty().bind(state.actionProperty().asString());
         actionLabel.setFont(actionFont);
-        //VBox labels = new VBox(idLabel, actionLabel);
         getChildren().addAll(icon, idLabel, actionLabel);
         setAlignment(idLabel, Pos.BOTTOM_CENTER);
         setAlignment(actionLabel, Pos.CENTER);
+
+        // Position and initialize
         setTranslateX(x);
         setTranslateY(y);
-        makeDraggable();
+        enableEdit();
+        enableEditContextMenu();
     }
 
-    private void makeDraggable() {
-        setOnMouseEntered(mouseEvent -> setCursor(Cursor.HAND));
-        setOnMouseExited(mouseEvent -> setCursor(Cursor.DEFAULT));
+    public void highlight() {
+        icon.setFill(Color.GOLD);
+    }
 
+    public void unhighlight() {
+        icon.setFill(Color.WHITE);
+    }
+
+    public void select() {
+        this.setFocused(true);
+    }
+
+    public void deselect() {
+        this.setFocused(false);
+    }
+
+    public void enableEdit() {
+        this.setCursor(Cursor.OPEN_HAND);
+        actionLabel.setCursor(Cursor.HAND);
+
+        enableEditContextMenu();
+        enableDragAndDrop();
+    }
+
+    private void enableEditContextMenu() {
+
+        MenuItem addTransition = new MenuItem("Add Transition");
+        MenuItem editState = new MenuItem("Edit State");
+        MenuItem editID = new MenuItem("Edit ID");
+        MenuItem delete = new MenuItem("Delete...");
+        delete.setDisable(true);
+
+        ContextMenu editContextMenu = new ContextMenu(addTransition,
+                editState, editID, delete);
+
+        actionLabel.setOnMouseClicked(e -> {
+            if (e.isStillSincePress())
+                editContextMenu.show(actionLabel, Side.BOTTOM, 0, 0);
+        });
+    }
+
+    private void enableDragAndDrop() {
         setOnMousePressed(mouseEvent -> {
             setCursor(Cursor.CLOSED_HAND);
             startDragX = mouseEvent.getSceneX() - getLayoutX();
@@ -55,14 +106,21 @@ public class Vertex extends StackPane {
             setLayoutY(mouseEvent.getSceneY() - startDragY);
         });
 
-        setOnMouseReleased(e -> setCursor(Cursor.OPEN_HAND));
+        setOnMouseReleased(mouseEvent -> setCursor(Cursor.OPEN_HAND));
     }
 
-    public void highlight() {
-        icon.setFill(Color.GOLD);
-    }
+    public void enableActionContextMenu() {
+        ContextMenu actionContextMenu = new ContextMenu();
 
-    public void unhighlight() {
-        icon.setFill(Color.WHITE);
+        Arrays.stream(State.Action.values())
+                .forEach(a -> {
+                    MenuItem m = new MenuItem(a.toString());
+                    m.setOnAction(e -> {
+                        state.setAction(a);
+                    });
+                    actionContextMenu.getItems().add(m);
+                });
+
+        actionLabel.setContextMenu(actionContextMenu);
     }
 }
